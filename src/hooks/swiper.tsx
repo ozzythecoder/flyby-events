871bb1f1d1bfb195b80bdfb5e4bf2e71679526c5
August McAllister <august.chm@gmail.com>
2023-10-28 10:32:03 -0500

    Began implementing swipe element

---
+++ src/hooks/swiper.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { TouchEvent, useState } from "react";
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+interface SwipeInput {
+  onSwipeLeft?: () => void;
+  onSwipeRight?: () => void;
+  onSwipeUp?: () => void;
+  onSwipeDown?: () => void;
+}
+
+interface SwipeOutput {
+  onTouchStart: (e: TouchEvent) => void;
+  onTouchMove: (e: TouchEvent) => void;
+  onTouchEnd: () => void;
+}
+
+export default function Swiper(input: SwipeInput): SwipeOutput {
+  const [touchStart, setTouchStart] = useState<Position>({ x: 0, y: 0 });
+  const [touchEnd, setTouchEnd] = useState<Position>({ x: 0, y: 0 });
+
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e: TouchEvent) => {
+    setTouchEnd({ x: 0, y: 0 });
+    setTouchStart({
+      x: e.targetTouches[0].clientX,
+      y: e.targetTouches[0].clientY,
+    });
+  };
+
+  const onTouchMove = (e: TouchEvent) => {
+    setTouchEnd({
+      x: e.targetTouches[0].clientX,
+      y: e.targetTouches[0].clientY,
+    });
+  };
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const xDistance = touchStart.x - touchEnd.x;
+    const yDistance = touchStart.y - touchEnd.y;
+
+    const isHorizontalSwipe = xDistance > yDistance;
+    const isVerticalSwipe = yDistance > xDistance;
+
+    if (isHorizontalSwipe) {
+      const isLeftSwipe = xDistance > minSwipeDistance;
+      const isRightSwipe = xDistance < -minSwipeDistance;
+
+      if (isLeftSwipe && input.onSwipeLeft) {
+        input.onSwipeLeft();
+      } else if (isRightSwipe && input.onSwipeRight) {
+        input.onSwipeRight();
+      }
+    } else if (isVerticalSwipe) {
+      const isUpSwipe = yDistance < -minSwipeDistance;
+      const isDownSwipe = yDistance > minSwipeDistance;
+
+      if (isUpSwipe && input.onSwipeUp) {
+        input.onSwipeUp();
+      } else if (isDownSwipe && input.onSwipeDown) {
+        input.onSwipeDown();
+      }
+    }
+  };
+
+  return {
+    onTouchStart,
+    onTouchMove,
+    onTouchEnd,
+  };
+}
