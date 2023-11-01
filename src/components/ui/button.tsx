e63b34cf601cd88b67f3b8c00150983b7c140be6
August McAllister <august.chm@gmail.com>
2023-11-01 15:58:48 -0500

    Began building style system with button
    Using class variance authority, clsx, and tailwind merge, the button.tsx component can define some default styles and simple variants.

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,64 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
+
+const buttonClasses = cva(
+  "p-2 min-w-[100px] inline-flex justify-around align-center",
+  {
+    variants: {
+      intent: {
+        primary: [
+          "bg-primary-600",
+          "text-text-100",
+          "outline",
+          "outline-background-transparent/30",
+        ],
+        secondary: [
+          "bg-secondary-300",
+          "text-text-900",
+          "outline",
+          "outline-background-transparent/30",
+        ],
+      },
+      shape: {
+        square: ["rounded-none"],
+        rounded: ["rounded-md"],
+      },
+      text: {
+        stylized: ["font-heading", "text-3xl"],
+        plain: ["font-body", "text-2xl"],
+      },
+      hover: {
+        static: [],
+        highlight: ["hover:scale-[1.15] transition-all"],
+      },
+    },
+    defaultVariants: {
+      intent: "primary",
+      text: "plain",
+      shape: "rounded",
+      hover: "static",
+    },
+  },
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonClasses> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  intent,
+  text,
+  hover,
+  className,
+}) => (
+  <button
+    className={twMerge(clsx(buttonClasses({ intent, text, hover }), className))}
+  >
+    {children}
+  </button>
+);
