efef20046b5cd23fca9f778e870bf54ea4d26d55
August McAllister <august.chm@gmail.com>
2023-11-01 12:02:35 -0500

    Added onFocus and onBlur properties to theme toggle button

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -3,14 +3,17 @@
 import { useThemeContext } from "@/context/theme";
 import { Sun, Moon } from "lucide-react";
 
-export default function ThemeToggle() {
+export default function ThemeToggle(props: {
+  onFocus?: () => void;
+  onBlur?: () => void;
+}) {
   const { theme, setTheme } = useThemeContext();
 
   const toggleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
   return (
-    <button onClick={toggleTheme}>
+    <button onClick={toggleTheme} {...props}>
       {theme === "dark" ? (
         <Moon className="text-primary-800 hover:text-primary-950 hover:scale-125 transition-all" />
       ) : (
