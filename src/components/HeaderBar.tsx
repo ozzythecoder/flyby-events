f57609742136dee15b2bc0fae29367aa450b524c
August McAllister <august.chm@gmail.com>
2023-11-01 12:02:18 -0500

    Removed links from top bar
    Will be focusing on side menu instead.

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,35 +1,13 @@
 import ThemeToggle from "./ThemeToggle";
-
-type Link = {
-  name: string;
-  url: string;
-  target: "_self" | "_blank"; // _self opens in current window, _blank opens in new window
-};
-
-const links: Link[] = [
-  {
-    name: "Home",
-    url: "#",
-    target: "_self",
-  },
-  {
-    name: "Test",
-    url: "#",
-    target: "_self",
-  },
-  {
-    name: "About Us",
-    url: "#",
-    target: "_blank",
-  },
-];
+import { links } from "@/lib/navigation";
 
 export default function HeaderBar() {
   return (
-    <div className="flex flex-col sm:flex-row mx-[15%] justify-between">
+    <header className="flex flex-col sm:flex-row mx-[15%] justify-between">
       <h2 className="text-primary-800 hover:text-primary-950 hover:underline text-3xl text-center font-heading">
         <a href="/">FLYBY events</a>
       </h2>
+      {/* 
       <nav className="text-primary-800 font-body font-[600]">
         <ul className="flex-row justify-evenly items-center hidden sm:flex">
           {links.map((link) => (
@@ -44,7 +22,7 @@ export default function HeaderBar() {
           ))}
         </ul>
       </nav>
-      <ThemeToggle />
-    </div>
+         */}
+    </header>
   );
 }
