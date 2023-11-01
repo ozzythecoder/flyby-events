fa5602130b9d5ebf01ab3903c543c27fcb9f731d
August McAllister <august.chm@gmail.com>
2023-11-01 16:00:05 -0500

    Added background highlight to text; added two buttons to home page for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeaderBar from "@components/HeaderBar";
 import SkipToContent from "@components/SkipToContent";
 import SwipeMenu from "@components/SwipeMenu";
+import { Button } from "@components/ui/button";
 
 export default function Home() {
   return (
@@ -17,19 +18,30 @@ export default function Home() {
           <h1 className="my-32 font-heading text-5xl md:text-6xl lg:text-8xl text-center text-primary-800">
             Organize your event in seconds.
           </h1>
-          <p className="font-body text-md md:text-lg text-primary-950">
-            Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-            reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
-            ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
-            Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-            voluptate dolor minim nulla est proident. Nostrud officia pariatur
-            ut officia. Sit irure elit esse ea nulla sunt ex occaecat
-            reprehenderit commodo officia dolor Lorem duis laboris cupidatat
-            officia voluptate. Culpa proident adipisicing id nulla nisi laboris
-            ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo
-            ex non excepteur duis sunt velit enim. Voluptate laboris sint
-            cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-          </p>
+          <div className="p-4 bg-background-transparent/20 rounded-md drop-shadow-xl">
+            <p className="font-body text-md md:text-lg text-text-950">
+              Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+              reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
+              ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+              Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet
+              voluptate voluptate dolor minim nulla est proident. Nostrud
+              officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex
+              occaecat reprehenderit commodo officia dolor Lorem duis laboris
+              cupidatat officia voluptate. Culpa proident adipisicing id nulla
+              nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua
+              reprehenderit commodo ex non excepteur duis sunt velit enim.
+              Voluptate laboris sint cupidatat ullamco ut ea consectetur et est
+              culpa et culpa duis.
+            </p>
+          </div>
+          <div className="flex flex-row justify-evenly mt-4">
+            <Button intent="primary" text="stylized" hover="highlight">
+              Log In
+            </Button>
+            <Button intent="secondary" text="stylized" hover="highlight">
+              Learn More
+            </Button>
+          </div>
         </main>
       </div>
     </div>
