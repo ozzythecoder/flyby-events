3d5ff822ef2c65ca5d629f9100a00b8343b50f62
August McAllister <august.chm@gmail.com>
2023-10-31 19:15:19 -0500

    adjusted menu colors

--- src/components/SwipeMenu.tsx
+++ src/components/SwipeMenu.tsx
@@ -3,6 +3,7 @@ import useSwipe from "@/hooks/useSwipe";
 import { useState } from "react";
 import { X, MenuIcon } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
+import ThemeToggle from "./ThemeToggle";
 
 export default function SwipeMenu() {
   const [menuOpen, setMenuOpen] = useState<Boolean>(false);
@@ -16,24 +17,24 @@ export default function SwipeMenu() {
   return (
     <div
       {...swipeHandlers}
-      className={`absolute drop-shadow-4xl z-10 h-[100vh] w-[100vw]`}
+      className={`absolute drop-shadow-4xl h-[100vh] w-[15vw] `}
     >
       <button
         className="fixed m-8 h-8 w-fit bg-transparent text-center"
         onClick={() => setMenuOpen(!menuOpen)}
       >
         {menuOpen ? (
-          <X className="text-primary-800" />
+          <X className="text-primary-800 hover:text-primary-950 hover:scale-125 transition-all" />
         ) : (
-          <MenuIcon className="text-primary-800" />
+          <MenuIcon className="text-primary-800 hover:text-primary-950 hover:scale-125 transition-all" />
         )}
       </button>
       <div
-        className={`overflow-hidden bg-black h-[100vh] transition-[width] ${
+        className={`overflow-hidden bg-primary-200 h-[100vh] max-w-[400px] transition-[width] ${
           menuOpen ? "w-[60vw] sm:w-[40vw]" : "w-[0vw]"
         }`}
       >
-        <ul className="flex flex-col gap-4 text-xl mx-16 my-16 text-primary-800 whitespace-nowrap">
+        <ul className="flex flex-col gap-4 text-xl mx-16 my-16 text-primary-950 whitespace-nowrap">
           <li>thing 1</li>
           <li>thing 2</li>
           <li>thing 3</li>
