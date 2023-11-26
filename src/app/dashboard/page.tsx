dfa07b3269bed8c497dac3c7428b5534e22b0f11
August McAllister <august.chm@gmail.com>
2023-11-26 17:55:14 -0600

    fix: removed erroneous children prop from dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,4 @@
-interface PageProps {
-  children: React.ReactNode;
-}
-
-export default function Dashboard(props: PageProps) {
+export default function Dashboard() {
   return (
     <main>
       <h1 className="font-heading text-4xl text-primary-600">Dashboard</h1>
