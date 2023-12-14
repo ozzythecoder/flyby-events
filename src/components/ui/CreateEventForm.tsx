98a20f9ecd14ac60a44f6cf5f7b88af8702bfb10
August McAllister <august.chm@gmail.com>
2023-12-13 20:40:39 -0600

    Created event form placeholder

---
+++ src/components/ui/CreateEventForm.tsx
@@ -0,0 +1,19 @@
+
+// TODO:
+// * name
+// * private
+// * description
+// * location
+// * date and time
+// * ticket link
+
+export default function CreateEventForm() {
+
+  return (
+    <form>
+      <div>
+
+      </div>
+    </form>
+  )
+}
