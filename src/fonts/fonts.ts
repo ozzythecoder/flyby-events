6b5ab9195fdcb3e7ea70853124d20a72c5508471
August McAllister <august.chm@gmail.com>
2023-12-14 16:02:23 -0600

    Moved fonts to own folder

---
+++ src/fonts/fonts.ts
@@ -0,0 +1,17 @@
+import { Bebas_Neue, Maven_Pro } from "next/font/google";
+
+const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  variable: "--font-bebas-neue",
+});
+
+const mavenPro = Maven_Pro({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  variable: "--font-maven-pro",
+});
+
+export { bebasNeue, mavenPro }
