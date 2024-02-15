94f842ab3167037d721ff47100464b37792ae49b
August McAllister <august.chm@gmail.com>
2024-02-15 17:58:39 -0600

    Root layout now uses global config file and provider components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import { cookies } from "next/headers";
-import { ThemeContextProvider } from "@/context/theme";
-import { bebasNeue, mavenPro } from "@/fonts/fonts";
+import { fonts } from "@/fonts";
 import "./globals.css";
-import clsx from "clsx";
-import { validateTheme } from "@/lib/validateTheme";
-import App from "./App";
-
-const DEFAULT_THEME = "dark";
+import ThemeWrapper from "@components/ThemeWrapper";
+import Providers from "@/providers";
 
 export const metadata: Metadata = {
   title: "FlyBy Events",
@@ -19,18 +13,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const themeCookie = cookies().get("theme")?.value;
-  const theme = validateTheme(themeCookie, DEFAULT_THEME);
-
   return (
-    <ThemeContextProvider themeInput={theme}>
-      <ClerkProvider>
-        <App>
-          <body className={clsx(mavenPro.variable, bebasNeue.variable)}>
-            {children}
-          </body>
-        </App>
-      </ClerkProvider>
-    </ThemeContextProvider>
+    <Providers>
+      <html className={fonts}>
+        <ThemeWrapper>{children}</ThemeWrapper>
+      </html>
+    </Providers>
   );
 }
